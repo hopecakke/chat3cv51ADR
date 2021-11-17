3f462317767f3eef0d8403213a11c41c54ab4676
ANA DOROTEO <94026358+hopecakke@users.noreply.github.com>
2021-11-17 17:14:45 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,12 +1,21 @@
-/*  Initializa Firebase con la configuración del proyecto. Revisa la
- * configuración en tu servidor de Firebase.  */
-// @ts-ignore
-firebase.initializeApp({
-  apiKey: "AIzaSyAknrvw3brgo7qxEyMk3jzsE02CyJKWo5c",
-  authDomain: "fbauten.firebaseapp.com",
-  projectId: "fbauten",
-  storageBucket: "fbauten.appspot.com",
-  messagingSenderId: "902838344264",
-  appId: "1:902838344264:web:221d59cd0ba838db98567d",
-  measurementId: "G-52E0LME3E0"
-    });
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDRsaSa7LkimyjlAEvfjZpBX2Qe_zA8gPU",
+  authDomain: "proyec-auten.firebaseapp.com",
+  projectId: "proyec-auten",
+  storageBucket: "proyec-auten.appspot.com",
+  messagingSenderId: "683481567401",
+  appId: "1:683481567401:web:0557c47b57021e98fe59b8",
+  measurementId: "G-P3RFYSJNY3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
